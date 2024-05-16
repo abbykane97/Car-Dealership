f2d87298ce4b29b55adcc61467b4066de547d030
abbkane97 <kanehaby20@gmail.com>
2024-05-16 08:14:58 -0400

    Final commit

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,14 +1,9 @@
 package com.pluralsight;
-
 import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
-        try {
-            userInterface.display();
-        } catch (IOException e) {
-            System.out.println("Error"+ e.getMessage());
-        }
+
     }
 }
